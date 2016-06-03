01b4d60e458763a2cadc98eda8d5986fe0aee150
Ellery <383188743@qq.com>
2016-06-03 18:21:15 +0800

    bugfix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -60,7 +60,5 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-
-//app.listen(3000);
 
 module.exports = server;
