75a58c352da9a9e64425f6b63fad4d757de10f6e
Ellery <383188743@qq.com>
2016-06-03 17:18:24 +0800

    bugfix

--- ssl/generate.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Ellery1 on 15/9/29.
- */
-var fs = require('fs'),
-    content = fs.readFileSync('server.key', 'utf-8');
-
-var js = "module.exports=\"" + content.replace(/([\n\r])/g, "\\n\"+\n\"") + "\";";
-
-fs.writeFileSync('serverkey.js', js);
