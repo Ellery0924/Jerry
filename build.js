eb32715cd28a2a66d2661fb3d5701819361b4ee3
Ellery <383188743@qq.com>
2016-06-06 14:16:31 +0800

    bugfix

---
+++ build.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Ellery1 on 16/6/6.
+ */
+var fs = require('fs'),
+    execSync = require('child_process').execSync,
+    crypto = require('crypto');
+
+var dest = './public/dest',
+    baseJs = dest + '/base.min.js',
+    indexJs = dest + '/index.js',
+    style = dest + '/style.min.css';
+
+execSync('grunt babel');
+execSync('webpack -p');
+execSync('grunt build');
+
+function md5(content) {
+
+    return crypto.createHash('md5').update(content).digest('hex');
+}
+
+var baseMd5 = md5(fs.readFileSync(baseJs)),
+    indexMd5 = md5(fs.readFileSync(indexJs)),
+    styleMd5 = md5(fs.readFileSync(style));
+
+fs.renameSync(baseJs, baseJs.replace('.js', '@' + baseMd5 + '.js'));
+fs.renameSync(indexJs, indexJs.replace('.js', '@' + indexMd5 + '.js'));
+fs.renameSync(style, style.replace('.css', '@' + styleMd5 + '.css'));
+
+fs.writeFileSync('./ver.json', JSON.stringify({
+    base: '@' + baseMd5,
+    index: '@' + indexMd5,
+    style: '@' + styleMd5
+}, null, 4));
+
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 var express = require('express'),
     router = express.Router();
 
+var ver = require('../ver.json');
+
 router.get('/qproxy', function (req, res) {
-    res.render('index');
+
+    res.render('index', {
+        indexVer: ver.index,
+        baseVer: ver.base,
+        styleVer: ver.style,
+        isDebug: false
+    });
+});
+
+router.get('/qproxyDev', function (req, res) {
+
+    res.render('index', {
+        baseVer: ver.base,
+        styleVer: ver.style,
+        isDebug: true
+    })
 });
 
 module.exports = router;
