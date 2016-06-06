f19db251a1907b800a78b2b6c17fa587af4efd95
Ellery <383188743@qq.com>
2016-06-06 17:52:58 +0800

    bugfix

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {maxage: 1000 * 60 * 60 * 30}));
 
 app.use('/', routes);
 app.use(ajax);
