f578c7f639d984e66e07fefb8614bad65907c9b1
Ellery <383188743@qq.com>
2016-06-21 12:14:36 +0800

    bugfix

--- public/es6/view/HostManage/GroupNavigator.js
+++ public/es6/view/HostManage/GroupNavigator.js
@@ -29,7 +29,7 @@ export default React.createClass({
     render(){
 
         const {onSwitchHttps, httpsOn}=this.props;
-        const httpsOnTxt = httpsOn === undefined ? '' : httpsOn ? '开启' : '关闭';
+        const httpsOnTxt = httpsOn === undefined || httpsOn === null ? '' : httpsOn ? '开启' : '关闭';
 
         return (
             <div className="col-sm-3 col-md-2 sidebar">
