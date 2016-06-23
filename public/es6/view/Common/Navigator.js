4d2e1b4db0c9f0dfaa654f4b0f571cca0cbb6303
Ellery <383188743@qq.com>
2016-06-23 19:46:01 +0800

    Merge branch 'master' into blockpoint-v2
    # Conflicts:
    #	public/dest/index.js
    #	public/es6/view/HostManage/GroupNavigator.js

--- public/es6/view/Common/Navigator.js
+++ public/es6/view/Common/Navigator.js
@@ -17,9 +17,9 @@ export default React.createClass({
                     <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
                             data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                         <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
+                        <span className="icon-bar"/>
+                        <span className="icon-bar"/>
+                        <span className="icon-bar"/>
                     </button>
                     <a className="navbar-brand" href="javascript:void 0;">Jerry</a>
                 </div>
