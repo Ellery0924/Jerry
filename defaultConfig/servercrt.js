cacbe2c9f907c4ce685a95105729da03edba55c9
Ellery <383188743@qq.com>
2016-06-23 14:25:49 +0800

    Merge branch 'master' into blockpoint-v2
    # Conflicts:
    #	public/dest/index.js
    #	public/es6/view/HostManage/GroupNavigator.js

--- defaultConfig/servercrt.js
+++ defaultConfig/servercrt.js
@@ -1,82 +1,17 @@
-module.exports="Certificate:\n"+
-"    Data:\n"+
-"        Version: 3 (0x2)\n"+
-"        Serial Number: 2 (0x2)\n"+
-"    Signature Algorithm: sha1WithRSAEncryption\n"+
-"        Issuer: C=CN, ST=Beijing, L=Beijing, O=Qunar, OU=qunarzz.com\n"+
-"        Validity\n"+
-"            Not Before: Apr 23 11:55:05 2014 GMT\n"+
-"            Not After : Apr 23 11:55:05 2015 GMT\n"+
-"        Subject: C=CN, ST=Beijing, O=Qunar, OU=qunarzz.com, CN=qunarzz.com\n"+
-"        Subject Public Key Info:\n"+
-"            Public Key Algorithm: rsaEncryption\n"+
-"                Public-Key: (2048 bit)\n"+
-"                Modulus:\n"+
-"                    00:c9:22:a5:45:e4:74:17:6b:f2:9f:0b:6f:b1:2b:\n"+
-"                    61:f5:e9:44:a3:2e:6b:70:b1:ad:6d:a0:92:c0:45:\n"+
-"                    bc:4f:ae:5e:14:5a:bf:30:77:29:e6:ae:e6:3b:10:\n"+
-"                    63:0f:e9:d0:d3:32:84:f5:f0:29:05:b1:c6:65:43:\n"+
-"                    aa:91:de:20:25:9e:06:99:88:6c:60:ac:b2:ad:d8:\n"+
-"                    f2:94:1a:89:cd:1b:e2:35:86:6d:6b:f4:62:2a:23:\n"+
-"                    d3:43:4f:9e:7c:43:3e:6f:f6:84:43:08:86:22:18:\n"+
-"                    d4:48:42:9e:66:a3:6b:2d:59:0f:87:f9:47:29:69:\n"+
-"                    b1:df:ca:74:02:1f:6f:d0:5a:72:d1:04:8f:05:57:\n"+
-"                    5a:34:38:bd:4e:77:07:ae:ab:e3:c0:08:fd:0d:ac:\n"+
-"                    cd:01:b7:44:ad:06:57:58:7b:e7:f0:65:e2:2d:b6:\n"+
-"                    77:fe:9e:4a:b4:dd:10:62:e4:4a:74:10:de:be:f5:\n"+
-"                    cc:a0:78:75:c0:58:bd:94:b5:50:12:ae:62:c8:31:\n"+
-"                    07:a3:0f:89:1c:49:28:16:cf:1e:e4:1e:59:cb:b5:\n"+
-"                    33:95:ee:77:ee:4e:26:bd:5a:9c:04:74:c7:25:41:\n"+
-"                    47:3a:f8:a4:7a:e4:31:66:0d:44:9b:b0:6b:37:7e:\n"+
-"                    f4:0d:6a:96:0a:b7:f6:e0:f7:c4:82:33:f8:01:9a:\n"+
-"                    1c:53\n"+
-"                Exponent: 65537 (0x10001)\n"+
-"        X509v3 extensions:\n"+
-"            X509v3 Basic Constraints: \n"+
-"                CA:FALSE\n"+
-"            Netscape Comment: \n"+
-"                OpenSSL Generated Certificate\n"+
-"            X509v3 Subject Key Identifier: \n"+
-"                7A:B3:BD:E2:FD:8D:1C:3B:FB:44:8E:5E:20:C4:7F:3F:B3:5A:D6:9D\n"+
-"            X509v3 Authority Key Identifier: \n"+
-"                keyid:7B:99:DE:B3:3D:A2:5C:B9:D9:DB:F2:4F:8B:93:47:69:AB:B7:F0:2F\n"+
-"\n"+
-"    Signature Algorithm: sha1WithRSAEncryption\n"+
-"         8d:0a:5c:54:ee:80:00:0c:c7:a8:9e:b3:ad:78:4b:82:83:42:\n"+
-"         f2:a6:84:3e:e2:d5:82:6c:ea:a8:5b:8e:26:0f:e5:f6:6a:d2:\n"+
-"         11:62:7f:40:fb:7b:4d:0c:b1:64:68:97:a0:5f:75:84:cf:37:\n"+
-"         0a:4f:e2:1c:ac:ca:73:f0:9b:d4:75:d8:0d:0a:99:64:11:3e:\n"+
-"         6f:5b:e9:26:2c:7c:99:fe:f4:ad:e9:10:ce:34:67:eb:f2:27:\n"+
-"         05:b3:19:b4:f7:6b:11:65:52:37:34:9c:de:d2:23:98:14:df:\n"+
-"         bd:c7:13:8d:5a:80:ee:e3:6a:bd:ec:77:ae:10:ca:54:ad:8d:\n"+
-"         cb:20:22:0f:ad:b5:ce:47:7e:36:0e:7f:f5:c8:9d:19:f6:94:\n"+
-"         54:dd:d7:37:da:de:c6:81:64:3b:24:db:6c:6b:51:0a:41:ae:\n"+
-"         d0:da:35:39:a6:bb:ba:7f:ba:72:94:c8:c5:a2:ac:8a:4e:00:\n"+
-"         fc:87:b1:83:ef:63:7b:e9:97:d3:1f:4e:f5:f6:4a:fc:78:5b:\n"+
-"         01:19:6c:77:ae:40:35:c5:fd:1e:97:48:c1:61:10:5c:8f:f0:\n"+
-"         ff:ed:46:1c:74:4f:a7:b6:65:c0:8e:c8:0f:32:82:37:3d:78:\n"+
-"         01:b7:c4:a1:1d:d1:34:f9:c0:5c:71:2d:03:90:71:19:02:78:\n"+
-"         f0:c3:f4:de\n"+
-"-----BEGIN CERTIFICATE-----\n"+
-"MIIDqDCCApCgAwIBAgIBAjANBgkqhkiG9w0BAQUFADBXMQswCQYDVQQGEwJDTjEQ\n"+
-"MA4GA1UECAwHQmVpamluZzEQMA4GA1UEBwwHQmVpamluZzEOMAwGA1UECgwFUXVu\n"+
-"YXIxFDASBgNVBAsMC3F1bmFyenouY29tMB4XDTE0MDQyMzExNTUwNVoXDTE1MDQy\n"+
-"MzExNTUwNVowWzELMAkGA1UEBhMCQ04xEDAOBgNVBAgMB0JlaWppbmcxDjAMBgNV\n"+
-"BAoMBVF1bmFyMRQwEgYDVQQLDAtxdW5hcnp6LmNvbTEUMBIGA1UEAwwLcXVuYXJ6\n"+
-"ei5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDJIqVF5HQXa/Kf\n"+
-"C2+xK2H16USjLmtwsa1toJLARbxPrl4UWr8wdynmruY7EGMP6dDTMoT18CkFscZl\n"+
-"Q6qR3iAlngaZiGxgrLKt2PKUGonNG+I1hm1r9GIqI9NDT558Qz5v9oRDCIYiGNRI\n"+
-"Qp5mo2stWQ+H+UcpabHfynQCH2/QWnLRBI8FV1o0OL1Odweuq+PACP0NrM0Bt0St\n"+
-"BldYe+fwZeIttnf+nkq03RBi5Ep0EN6+9cygeHXAWL2UtVASrmLIMQejD4kcSSgW\n"+
-"zx7kHlnLtTOV7nfuTia9WpwEdMclQUc6+KR65DFmDUSbsGs3fvQNapYKt/bg98SC\n"+
-"M/gBmhxTAgMBAAGjezB5MAkGA1UdEwQCMAAwLAYJYIZIAYb4QgENBB8WHU9wZW5T\n"+
-"U0wgR2VuZXJhdGVkIENlcnRpZmljYXRlMB0GA1UdDgQWBBR6s73i/Y0cO/tEjl4g\n"+
-"xH8/s1rWnTAfBgNVHSMEGDAWgBR7md6zPaJcudnb8k+Lk0dpq7fwLzANBgkqhkiG\n"+
-"9w0BAQUFAAOCAQEAjQpcVO6AAAzHqJ6zrXhLgoNC8qaEPuLVgmzqqFuOJg/l9mrS\n"+
-"EWJ/QPt7TQyxZGiXoF91hM83Ck/iHKzKc/Cb1HXYDQqZZBE+b1vpJix8mf70rekQ\n"+
-"zjRn6/InBbMZtPdrEWVSNzSc3tIjmBTfvccTjVqA7uNqvex3rhDKVK2NyyAiD621\n"+
-"zkd+Ng5/9cidGfaUVN3XN9rexoFkOyTbbGtRCkGu0No1Oaa7un+6cpTIxaKsik4A\n"+
-"/Iexg+9je+mX0x9O9fZK/HhbARlsd65ANcX9HpdIwWEQXI/w/+1GHHRPp7ZlwI7I\n"+
-"DzKCNz14AbfEoR3RNPnAXHEtA5BxGQJ48MP03g==\n"+
+module.exports="-----BEGIN CERTIFICATE-----\n"+
+"MIICdzCCAeACCQDznKp7pVKc6DANBgkqhkiG9w0BAQUFADCBgTELMAkGA1UEBhMC\n"+
+"Q04xEDAOBgNVBAgTB0JlaWppbmcxEDAOBgNVBAcTB0JlaWppbmcxDDAKBgNVBAoT\n"+
+"A1FOUjEMMAoGA1UECxMDRFpTMREwDwYDVQQDEwhzaGVuamlhbzEfMB0GCSqGSIb3\n"+
+"DQEJARYQMzgzMTg4NzQzQHFxLmNvbTAeFw0xNjA2MjMwNjE3MDNaFw0xNjA3MjMw\n"+
+"NjE3MDNaMH4xCzAJBgNVBAYTAkNOMRAwDgYDVQQIEwdCZWlqaW5nMRAwDgYDVQQH\n"+
+"EwdCZWlqaW5nMQwwCgYDVQQKEwNRTlIxDDAKBgNVBAsTA0RaUzEOMAwGA1UEAxMF\n"+
+"amVycnkxHzAdBgkqhkiG9w0BCQEWEDM4MzE4ODc0M0BxcS5jb20wgZ8wDQYJKoZI\n"+
+"hvcNAQEBBQADgY0AMIGJAoGBAKhz3pgevU9i9UlLT9pyWqHsFAmumIZGJbXFbR0O\n"+
+"EGPB6zxXK3Zmod0WJ6SY4gbTqmhruHVnlgBWrRIezUVCSWmiEtEgaZaSHkKUDsKU\n"+
+"7dz/Fg+wMC7bbkNup6Tqs5aYfSxr9rJEmzeDXKClYcT3puj8cmqP4i67BBP7qEeZ\n"+
+"NVezAgMBAAEwDQYJKoZIhvcNAQEFBQADgYEAiz85IrXjnIaBbh2a+2o/bPjZfEF5\n"+
+"HAJPNYL4zUNIVnaalYGrR1SN2BbrbWYYxzJhyNPPukFu8JhQY08IfehMhf5FoAah\n"+
+"8tuG/mvTBnymNmm8ubzNM3gx3pUyteVqqwGcqZ2Z1blmwQkVUcKG3vFwGK3LFYYy\n"+
+"1iqki3GAQHWTjhc=\n"+
 "-----END CERTIFICATE-----\n"+
 "";
