f74f220111880023670040e5723ab38665a3a661
jiao.shen <jiao.shen@qunar.com>
2016-07-30 00:30:52 +0800

    消灭es6代码

--- server_modules/web/app.js
+++ server_modules/web/app.js
@@ -14,11 +14,9 @@ var app = express();
 var server = http.createServer(app);
 var publicPath = path.resolve(__dirname, '..', '..', 'public');
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(publicPath, 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -29,16 +27,12 @@ app.use(express.static(publicPath, {maxage: 1000 * 60 * 60 * 30}));
 app.use('/', routes);
 app.use(ajax);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
@@ -49,8 +43,6 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,28 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: "./public/scripts/index.js",
+    entry: "./public/es6/index.js",
     output: {
         path: "./public/dest/",
         filename: "index.js"
-    }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        })
+    ]
 };
