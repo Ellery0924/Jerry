6067252bb10dcbe22e24e5e486362d5f33cc5c21
jiao.shen <jiao.shen@qunar.com>
2016-08-04 14:35:40 +0800

    Merge branch 'networksetting'
    # Conflicts:
    #	public/dest/index.js

--- public/es6/wsClient.js
+++ public/es6/wsClient.js
@@ -5,6 +5,5 @@ import io from 'socket.io-client/socket.io';
 
 export default function(logServerPort){
 
-    console.log(logServerPort)
-    return new io('http://127.0.0.1:'+logServerPort)
+    return new io('http://127.0.0.1:'+logServerPort);
 }
