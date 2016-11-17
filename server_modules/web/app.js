9ae96a9b9cb66e302d197f3f9226e94f83753223
jiao.shen <jiao.shen@qunar.com>
2016-11-17 10:43:02 +0800

    优化ykit项目的host存储策略

--- server_modules/web/app.js
+++ server_modules/web/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var http = require('http');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -18,7 +17,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(publicPath, 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
@@ -32,16 +30,6 @@ app.use(function (req, res, next) {
     err.status = 404;
     next(err);
 });
-
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
 
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
