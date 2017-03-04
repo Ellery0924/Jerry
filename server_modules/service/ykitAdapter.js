da161c81c992cbfef632ae63f9e766b6ca1f23b3
Ellery0924 <383188743@qq.com>
2017-03-04 19:46:11 +0800

    rename files

--- server_modules/service/ykitAdapter.js
+++ server_modules/service/ykitAdapter.js
@@ -6,125 +6,12 @@ var requireUncached = require('require-uncached');
 var HOST_FILE_NAME = 'ykit.hosts',
     MOCK_FILE_NAME = 'mock.js',
     fs = require('fs'),
-    Path = require('path');
+    Path = require('path'),
+    formatRuleList = require('./hostUtils').formatRuleList;
 
 var rignore = /^\./,
     rykit = /\_ykit$/,
     rykitconfig = /ykit(?:\.[\w\_\d]+)?\.js/;
-
-function exportHostList(ruleList, serverInfo, shouldNotShowOnlineHosts) {
-    return ruleList && ruleList.length ? ruleList.reduce(function (result, rule) {
-            var domainArr = rule.domain.replace(/[\n\r\s]+/, ' ').split(/\s+/),
-                current = rule.current,
-                isOnline = current === 'online',
-                cache = rule.cache,
-                selectedServerIndex = cache[current],
-                ip = current === 'custom' ?
-                    cache.custom :
-                    serverInfo[current][selectedServerIndex];
-
-            if (shouldNotShowOnlineHosts) {
-                if (!isOnline) {
-                    result = result.concat(ip + ' ' + domainArr.join(' '));
-                }
-            } else {
-                result = result.concat((isOnline ? 'online' : ip) + ' ' + domainArr.join(' '));
-            }
-
-            return result;
-        }, []).join('\n') : '';
-}
-
-function getServerByIp(ip, serverInfo) {
-    var rshortcut = /(.+)\-(\d+)/,
-        mshortcut = ip.match(rshortcut);
-
-    if (mshortcut && mshortcut.length) {
-        var insertGroupName = mshortcut[1],
-            insertIpIndex = mshortcut[2],
-            targetGroup = serverInfo[insertGroupName];
-
-        if (targetGroup !== undefined) {
-            if (Object.keys(targetGroup).some(function (serverIndex) {
-                    return serverIndex === insertIpIndex
-                })) {
-                return {
-                    groupName: insertGroupName,
-                    ipIndex: insertIpIndex
-                };
-            }
-        }
-    }
-
-    var groupInfo;
-
-    for (var groupName in serverInfo) {
-        if (serverInfo.hasOwnProperty(groupName)) {
-            groupInfo = serverInfo[groupName];
-            for (var ipIndex in groupInfo) {
-                if (groupInfo.hasOwnProperty(ipIndex)) {
-                    if (groupInfo[ipIndex] === ip) {
-                        return {
-                            groupName: groupName,
-                            ipIndex: ipIndex
-                        };
-                    }
-                }
-            }
-        }
-    }
-
-    return ip !== 'online' ? {
-            groupName: 'custom',
-            ipIndex: ip
-        } : {
-            groupName: 'online'
-        };
-}
-
-function formatRuleListGenerator(validator) {
-    return function (ruleListStr, serverInfo, existedRuleList) {
-        var ruleListRaw = ruleListStr.replace(/\#.*([\n\r]|$)/g, '\n').split(/[\n\r]+/),
-            ruleList = ruleListRaw.reduce(function (acc, ruleStr) {
-                var ruleArr = ruleStr.trim().split(/\s+/),
-                    ip = ruleArr.shift(),
-                    domain = ruleArr.join(' ');
-
-                if (ip && domain) {
-                    acc.push({
-                        ip: ip,
-                        domain: domain
-                    });
-                }
-
-                return acc;
-            }, []);
-
-        if (validator) {
-            var validated = validator(ruleList, existedRuleList);
-            if (!validated.result) {
-                alert(validated.message);
-                return null;
-            }
-        }
-
-        return ruleList.map(function (rule) {
-            var targetServer = getServerByIp(rule.ip, serverInfo),
-                domain = rule.domain,
-                generatedRule = {
-                    domain: domain,
-                    current: targetServer.groupName,
-                    cache: {}
-                };
-
-            if (targetServer.groupName !== 'online') {
-                generatedRule.cache[targetServer.groupName] = targetServer.ipIndex;
-            }
-
-            return generatedRule;
-        });
-    }
-}
 
 function fetchGroupConfigFromYkitFolder(serverInfo, CWD) {
     return iterateFolder(CWD, function (acc, folder) {
