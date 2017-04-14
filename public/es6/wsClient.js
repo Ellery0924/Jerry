d0bb849634861e623f3e6994b8bf190f0632cd4f
ellery0924 <383188743@qq.com>
2017-04-14 10:01:42 +0800

    增加对ws的支持

--- public/es6/wsClient.js
+++ public/es6/wsClient.js
@@ -1,7 +1,7 @@
 /**
  * Created by Ellery1 on 16/6/19.
  */
-import io from 'socket.io-client/socket.io';
+import io from 'socket.io-client';
 
 export default function (logServerPort) {
     return new io('http://127.0.0.1:' + logServerPort);
